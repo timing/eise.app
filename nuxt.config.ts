e8d9e4904374d079a80e892b38829d02bc739fb7
Tijmen Ruizendaal <tijmen.ruizendaal@gmail.com>
2026-01-30 21:36:06 +0100

    Added simple analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 // also trying to fix this: https://github.com/nuxt/nuxt/issues/22141
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	app: {
+		head: {
+			script: [
+				{
+					src: 'https://scripts.simpleanalyticscdn.com/latest.js',
+					async: true,
+					'data-collect-dnt': 'true'
+				}
+			]
+		}
+	},
 	runtimeConfig: {
 		public: {
 			buildTimestamp: Date.now() // Unix timestamp in ms, set at build time
