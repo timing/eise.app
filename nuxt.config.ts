234c6581eb526a4c313aaa518c8c1b76f48a165b
Tijmen Ruizendaal <tijmen.ruizendaal@gmail.com>
2026-02-21 11:43:31 +0100

    Add llms.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	app: {
 		head: {
+			link: [
+				{ rel: 'alternate', type: 'text/markdown', href: '/llms.txt' }
+			],
 			meta: [
 				{ property: 'og:type', content: 'website' },
 				{ property: 'og:site_name', content: 'eise.app' },
